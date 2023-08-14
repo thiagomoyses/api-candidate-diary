3527dec69edc96598658c735ca4204f5e8907deb
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-08-14 17:57:27 +0200

    create auth controller

--- config/auth.php
+++ config/auth.php
@@ -43,7 +43,8 @@ return [
 
         'api' => [
             'driver' => 'jwt',
-            'provider' => 'users'
+            'provider' => 'users',
+            'hash' => false,
         ]
     ],
 
