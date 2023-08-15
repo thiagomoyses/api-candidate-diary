a99204834ab0cb9e64b942a370fc4825b5c5ea6f
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-08-15 13:23:50 +0200

    Create logic for authentication and authorization

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
-    //
+    
+    public function signup(Request $request) 
+    {
+        $input = $request->only('name', 'email', 'password', 'c_password');
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError($validator->errors(), 'Validation Error', 422);
+        }
+
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input); 
+
+        $success['user'] = $user;
+
+        return $this->sendResponse($success, 'user registered successfully', 201);
+
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+
+        $validator = Validator::make($input, [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError($validator->errors(), 'Validation Error', 422);
+        }
+
+        try {
+            // this authenticates the user details with the database and generates a token
+            if (! $token = JWTAuth::attempt($input)) {
+                return $this->sendError([], "invalid login credentials", 400);
+            }
+        } catch (JWTException $e) {
+            return $this->sendError([], $e->getMessage(), 500);
+        }
+
+        $success = [
+            'token' => $token,
+        ];
+        return $this->sendResponse($success, 'successful login', 200);
+    }
+
+    public function getUser() 
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            if (!$user) {
+                return $this->sendError([], "user not found", 403);
+            } 
+        } catch (JWTException $e) {
+            return $this->sendError([], $e->getMessage(), 500);
+        }
+
+        return $this->sendResponse($user, "user data retrieved", 200);
+    }
+
+
+
+    public function sendResponse($data, $message, $status = 200) 
+    {
+        $response = [
+            'data' => $data,
+            'message' => $message
+        ];
+
+        return response()->json($response, $status);
+    }
+
+    public function sendError($errorData, $message, $status = 500)
+    {
+        $response = [];
+        $response['message'] = $message;
+        if (!empty($errorData)) {
+            $response['data'] = $errorData;
+        }
+
+        return response()->json($response, $status);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,43 +19,42 @@ Route::middleware('api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', function(Request $request){
-    $credentials = $request->only(['email', 'password']);
+Route::prefix('auth')->group(function(){
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('auth.login');
+    Route::post('/signup', [\App\Http\Controllers\AuthController::class, 'signup'])->name('auth.signup');
 
-    if(!$token = auth('api')->attempt($credentials)){
-        abort(401);
-    }
-
-    return response()->json([
-        'data' => [
-            'token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
-        ]
-    ]);
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('/refresh', [\App\Http\Controllers\AuthController::class, 'refresh'])->name('auth.refresh');
 });
 
-Route::prefix('/candidates')->group(function(){
+
+Route::middleware('jwt.verify')->group(function() {
+    Route::get('/dashboard', function() {
+        return response()->json(['message' => 'Welcome to dashboard'], 200);
+    });
+});
+
+Route::middleware('jwt.verify')->prefix('/candidates')->group(function(){
     Route::get('/all', [\App\Http\Controllers\CandidatesController::class, 'index']);
     Route::post('/create', [\App\Http\Controllers\CandidatesController::class, 'create']);
     Route::patch('/update/{id}', [\App\Http\Controllers\CandidatesController::class, 'update'])->where('id', '[0-9]+');
 });
 
 
-Route::prefix('/companies')->group(function(){
+Route::middleware('jwt.verify')->prefix('/companies')->group(function(){
     Route::get('/all', [\App\Http\Controllers\CompaniesController::class, 'index']);
     Route::post('/create', [\App\Http\Controllers\CompaniesController::class, 'create']);
     Route::patch('/update/{id}', [\App\Http\Controllers\CompaniesController::class, 'update'])->where('id', '[0-9]+');
 });
 
-Route::prefix('/projects')->group(function() {
+Route::middleware('jwt.verify')->prefix('/projects')->group(function() {
     Route::get('/all', [\App\Http\Controllers\ProjectsController::class, 'index']);
     Route::post('/create', [\App\Http\Controllers\ProjectsController::class, 'create']);
     Route::patch('/update/{job_reference}', [\App\Http\Controllers\ProjectsController::class, 'update'])->where('job_reference', '[A-Za-z0-9]{10}');
     Route::post('/delete/{job_reference}', [\App\Http\Controllers\ProjectsController::class, 'delete'])->where('job_reference', '[A-Za-z0-9]{10}');
 });
 
-Route::prefix('/diary')->group(function (){
+Route::middleware('jwt.verify')->prefix('/diary')->group(function (){
     Route::get('/all', [\App\Http\Controllers\DiaryController::class, 'index']);
     Route::post('/create', [\App\Http\Controllers\DiaryController::class, 'create']);
     Route::patch('/update/{id}', [\App\Http\Controllers\DiaryController::class, 'update'])->where('id', '[0-9]+');
