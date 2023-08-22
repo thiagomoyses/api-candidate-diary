16f1743439a8c2434aef3fb7e40619f5d614d6c5
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-08-22 13:35:24 +0200

    create factory and seeder

---
+++ database/factories/CandidatesFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Candidates>
+ */
+class CandidatesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'phone' => $this->faker->tollFreePhoneNumber,
+            'email' => $this->faker->unique()->email,
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'country' => $this->faker->country(),
+            'surname' => $this->faker->lastName(),
+            'situation' => 1,
+            'client_id_fk' => '20230821294849'
+        ];
+    }
+}
